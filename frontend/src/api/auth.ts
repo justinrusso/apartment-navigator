19735608568ead421ade6984e83a5a2071f1befe
Justin Russo <j.russo@outlook.com>
2022-01-18 18:58:09 -0500

    feat(auth): create auth api helper func

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,18 @@
+import { fetchApi, routeBuilder } from "./util";
+
+const authRoute = routeBuilder("/api/auth");
+
+interface RegistrationData {
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  company?: string;
+}
+
+export async function register(data: RegistrationData) {
+  return fetchApi(authRoute("/register"), {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+}
---
+++ frontend/src/api/util.ts
@@ -0,0 +1,40 @@
+interface RequestOptions extends RequestInit {
+  headers?: Record<string, string>;
+}
+
+export async function fetchApi(
+  url: string,
+  options?: RequestOptions
+): Promise<Response> {
+  const revisedOptions = options || {};
+
+  if (revisedOptions?.method?.toUpperCase() !== "GET") {
+    if (!revisedOptions.headers) {
+      revisedOptions.headers = {};
+    }
+    revisedOptions.headers["Content-Type"] =
+      revisedOptions.headers["Content-Type"] || "application/json";
+  }
+
+  const res = await fetch(url, revisedOptions);
+
+  if (res.status >= 400) {
+    throw res;
+  }
+
+  return res;
+}
+
+/**
+ *
+ * @param basePath In the form of `/api/test`. There should not be a `/` at the end of the string!
+ * @returns a builder function to create the full path
+ */
+export function routeBuilder(basePath: string) {
+  return (path?: string): string => {
+    if (!path || path === "/") {
+      return basePath;
+    }
+    return `${basePath}/${path}`;
+  };
+}
