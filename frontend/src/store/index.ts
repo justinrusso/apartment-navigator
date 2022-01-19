847c5a448d51ff034f4fd64772f2840738ecf086
Justin Russo <j.russo@outlook.com>
2022-01-18 19:15:47 -0500

    feat: initialize redux

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,26 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import type { Middleware } from "redux";
+
+const isDev = process.env.NODE_ENV !== "production";
+
+const rootReducer = combineReducers({});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+const store = configureStore({
+  devTools: isDev,
+  middleware: (getDefaultMiddleware) => {
+    if (isDev) {
+      const logger = require("redux-logger").default;
+      return getDefaultMiddleware().concat(
+        logger as Middleware<any, RootState>
+      );
+    }
+    return getDefaultMiddleware();
+  },
+  reducer: rootReducer,
+});
+
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
