442ee743bdb526888d82f5cecb9fc921bf75d50a
Justin Russo <j.russo@outlook.com>
2022-01-19 19:24:15 -0500

    feat(auth): style modal switching links & demo login link

---
+++ frontend/src/components/auth/AlternateAuthLink.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const AlternateAuthLink = styled.button`
+  background: none;
+  border: none;
+  color: green; // TODO: use primary color
+  cursor: pointer;
+  outline: none;
+  padding: 0;
+`;
+
+export default AlternateAuthLink;
