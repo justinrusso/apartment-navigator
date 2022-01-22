7d309e3399ab9643be90e8c6ee18d0ea52fc32c8
Justin Russo <j.russo@outlook.com>
2022-01-22 14:55:58 -0500

    feat(properties): seed 6 properties

--- backend/app/seeds/__init__.py
+++ backend/app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from faker import Faker
 from flask.cli import AppGroup
 
+from .properties import seed_properties, undo_properties
 from .users import seed_users, undo_users
 
 
@@ -10,11 +11,12 @@ seed_commands = AppGroup("seed")
 @seed_commands.command("all")
 def seed():
     fake = Faker()
-    # Add seed functions here
+
     seed_users(fake)
+    seed_properties()
 
 
 @seed_commands.command("undo")
 def undo():
-    # Add undo functions here
     undo_users()
+    undo_properties()
