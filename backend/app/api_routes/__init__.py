405cd3e7e6c9bf4d9b4d9568eb9737dc3062957a
Justin Russo <j.russo@outlook.com>
2022-01-24 19:03:50 -0500

    feat(properties): add GET property unit categories route

--- backend/app/api_routes/__init__.py
+++ backend/app/api_routes/__init__.py
@@ -2,9 +2,11 @@ from flask import Blueprint
 
 from .auth import auth_routes
 from .properties import properties_routes
+from .units import units_routes
 
 
 api_routes = Blueprint("api", __name__, url_prefix="/api")
 
 api_routes.register_blueprint(auth_routes)
 api_routes.register_blueprint(properties_routes)
+api_routes.register_blueprint(units_routes)
