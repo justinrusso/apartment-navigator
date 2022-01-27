ef4ab9fef2cd52935f6af11c7fadb5a1a004847f
Justin Russo <j.russo@outlook.com>
2022-01-26 19:31:42 -0500

    fix(properties): add range of years to Year Built

--- backend/app/forms/property_form.py
+++ backend/app/forms/property_form.py
@@ -1,6 +1,7 @@
+from datetime import date
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SelectField, StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, NumberRange
 
 from app.forms import ListField
 
@@ -8,7 +9,9 @@ from app.forms import ListField
 class PropertyForm(FlaskForm):
     ownerId = IntegerField(validators=[DataRequired()])
     categoryId = SelectField(coerce=int, validators=[DataRequired()])
-    builtInYear = IntegerField(validators=[DataRequired()])
+    builtInYear = IntegerField(
+        validators=[DataRequired(), NumberRange(min=1800, max=date.today().year + 20)]
+    )
     name = StringField(validators=[])
     address1 = StringField(validators=[DataRequired()])
     address2 = StringField(validators=[])
