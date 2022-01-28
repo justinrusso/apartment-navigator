0c3e62e08eeb554bd925b61f11d0e5a03f719897
Justin Russo <j.russo@outlook.com>
2022-01-28 15:22:25 -0500

    feat(reviews): improve rating validations using select field choices

--- backend/app/forms/review_form.py
+++ backend/app/forms/review_form.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField
+from wtforms import SelectField, StringField
 from wtforms.validators import DataRequired
 
 
 class ReviewForm(FlaskForm):
-    rating = IntegerField(validators=[DataRequired()])
+    rating = SelectField(
+        coerce=int,
+        validators=[DataRequired()],
+        choices=[(i, i) for i in range(1, 6)],
+    )
     comment = StringField(validators=[DataRequired()])
